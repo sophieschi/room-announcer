a034a37c2673b68f073436d38977e747fd3d6152
Sophie Schiller <sophie.schiller01@gmail.com>
2018-08-18 19:31:20 +0200

    shuffle things around

--- node.lua
+++ node.lua
@@ -312,16 +312,16 @@ local content = switcher(function()
                 end
 
                 return function(y)
-                    CONFIG.font_text:write(70, y, talk.start_str, 40, CONFIG.fgcolor2.rgb_with_a(alpha))
-                    CONFIG.font_text:write(230, y, rooms[talk.place].name_short, 40, CONFIG.fgcolor2.rgb_with_a(alpha))
+                    CONFIG.font_text:write(70, y, talk.start_str, 45, CONFIG.fgcolor2.rgb_with_a(alpha))
+                    CONFIG.font_text:write(230, y, rooms[talk.place].name_short, 45, CONFIG.fgcolor2.rgb_with_a(alpha))
                     local line_y = y
                     for idx = 1, #talk.title_lines do
                         local title = talk.title_lines[idx]
                         CONFIG.font_text:write(CONFIG.text_offset, line_y, title, 25, CONFIG.fgcolor2.rgb_with_a(alpha))
-                        line_y = line_y + 24
+                        line_y = line_y + 23
                     end
                     CONFIG.font_text:write(CONFIG.text_offset, line_y, talk.speaker_line, 25, CONFIG.fgcolor2.rgb_with_a(alpha*0.6))
-                    line_y = line_y + 24
+                    line_y = line_y + 23
                     return math.max(60, line_y - y) + 4
                 end
             end
